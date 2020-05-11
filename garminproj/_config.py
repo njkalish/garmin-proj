328f70f7d56e19ae0d4a3530b2224abd9d569299
Nathan Kalish <32024095+njkalish@users.noreply.github.com>
2020-05-10 22:50:30 -0700

    attempt to fix Austin's config bug

--- garminproj/_config.py
+++ garminproj/_config.py
@@ -4,8 +4,8 @@ from pathlib import Path
 project_path = Path(__file__).parents[1]
 
 config = ConfigParser()
-config.read(project_path / 'config.ini')
+config.read([str(project_path / 'config.ini')])
 
 db_config = ConfigParser()
 
-db_config.read(project_path / 'alembic.ini')
+db_config.read([str(project_path / 'alembic.ini')])
